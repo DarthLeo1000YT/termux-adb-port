b03d765e69f9fa357c56c471831d2654ee12d2fa
DarthLeo1000YT <77907600+DarthLeo1000YT@users.noreply.github.com>
2026-05-10 09:57:03 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 download(){
-  wget -N $1 &>/dev/null;
+  wget --no-cache --post-data="action=purge" -N $1 &>/dev/null;
 };
 cd
 mkdir -p termux-adb-port
@@ -8,3 +8,4 @@ cd termux-adb-port
 download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port
 download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port_arm.bin
 download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port_arm64.bin
+download https://raw.githubusercontent.com/DarthLeo1000YT/termux-adb-port/main/termux-adb-port
