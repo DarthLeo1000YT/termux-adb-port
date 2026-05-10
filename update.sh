1f1d4f9e1a54c57a337c464a2473ffd586505e4d
DarthLeo1000YT <77907600+DarthLeo1000YT@users.noreply.github.com>
2026-05-10 10:03:37 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 download(){
   echo "Downloading $2 from GitHub."
-  wget --no-cache --post-data="action=purge" $1 -O $2 &>/dev/null;
+  wget --no-cache --post-data="action=purge" "https://raw.githubusercontent.com/$1" -O $2 &>/dev/null;
 };
 cd
 mkdir -p termux-adb-port
 cd termux-adb-port
-download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port adbw_port
-download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port_arm.bin adbw_port_arm.bin
-download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port_arm64.bin adbw_port_arm64.bin
-download https://raw.githubusercontent.com/DarthLeo1000YT/termux-adb-port/main/termux-adb-port termux-adb-port
+download IMXEren/automation/main/adb-wifi/port-discovery/adbw_port adbw_port
+download IMXEren/automation/main/adb-wifi/port-discovery/adbw_port_arm.bin adbw_port_arm.bin
+download IMXEren/automation/main/adb-wifi/port-discovery/adbw_port_arm64.bin adbw_port_arm64.bin
+download DarthLeo1000YT/termux-adb-port/main/termux-adb-port termux-adb-port
