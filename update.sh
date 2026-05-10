fc0968f6b89ea325f3dba92d8dc0ce9711160635
DarthLeo1000YT <77907600+DarthLeo1000YT@users.noreply.github.com>
2026-05-10 10:00:31 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 download(){
-  wget --no-cache --post-data="action=purge" -N $1 &>/dev/null;
+  wget --no-cache --post-data="action=purge" $1 -O $2 &>/dev/null;
 };
 cd
 mkdir -p termux-adb-port
 cd termux-adb-port
-download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port
-download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port_arm.bin
-download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port_arm64.bin
-download https://raw.githubusercontent.com/DarthLeo1000YT/termux-adb-port/main/termux-adb-port
+download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port adbw_port
+download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port_arm.bin adbw_port_arm.bin
+download https://raw.githubusercontent.com/IMXEren/automation/main/adb-wifi/port-discovery/adbw_port_arm64.bin adbw_port_arm64.bin
+download https://raw.githubusercontent.com/DarthLeo1000YT/termux-adb-port/main/termux-adb-port termux-adb-port
